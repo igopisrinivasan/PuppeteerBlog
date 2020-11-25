f013dbaab89a7edebceea290db5c0c2ffc68ed07
Gopi Srinivasan <igopisrinivasan@gmail.com>
2020-11-26 01:19:56 +0530

    Working with Browsers and Pages in Puppeteer

--- eatest.js
+++ eatest.js
@@ -1,9 +1,10 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
+  const browser = await puppeteer.launch({ headless: false, slowMo: 50 });
+  const context = await browser.createIncognitoBrowserContext();
+  const page = await context.newPage();
   await page.goto("https://demosite.executeautomation.com/Login.html");
-
+  await page.type("[name=UserName]", "executeautomation");
   await browser.close();
 })();
